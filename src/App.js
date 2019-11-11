354b8265d0c8412a96fb62a4f913cb3193738050
Ryan Boris <ryan.boris@gmail.com>
2019-11-11 17:25:58 -0500

    :art: Adding firebase crud

--- src/App.js
+++ src/App.js
@@ -1,5 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { db } from './index'
+import uuidv4 from 'uuid'
 
-const App = () => <div>App renders!</div>
+const App = () => {
+  const [users, setUsers] = useState([])
+  const [first, setFirst] = useState('')
+  const [last, setLast] = useState('')
+  const [age, setAge] = useState('')
+
+  useEffect(() => {}, [])
+
+  async function postUser(e) {
+    e.preventDefault()
+    const user = db.collection('users').doc()
+    await user.set({
+      firstName: first,
+      lastName: last,
+      age,
+      timestamp: new Date().toLocaleTimeString()
+    })
+    setFirst('')
+    setLast('')
+    setAge('')
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={postUser}>
+        <input
+          type="text"
+          placeholder="first name"
+          value={first}
+          onChange={e => setFirst(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="last name"
+          value={last}
+          onChange={e => setLast(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="age"
+          value={age}
+          onChange={e => setAge(e.target.value)}
+        />
+        <input type="submit" value="submit" />
+      </form>
+      <hr />
+      <div>
+        {users.map(user => (
+          <div key={uuidv4()}>
+            <p>{user.firstName}</p>
+            <p>{user.lastName}</p>
+            <p>{user.age}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
+import firebase from 'firebase'
+import firebaseConfig from './creds'
+
+// initialize FB app
+firebase.initializeApp(firebaseConfig)
+
+// initialize the db
+
+export const db = firebase.firestore()
 
 render(<App />, document.getElementById('root'))
